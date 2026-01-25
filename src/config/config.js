8a8fc415844c61e321b36db9571712404ca63230
sachin-kumar05 <sach7870@gmail.com>
2026-01-25 17:19:59 +0530

    feat: update dependencies and add config for environment variables

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+// This is production grade approach for env variables Here we have wrapped each env variables in String bcz sometime if can be treated as number if there is only numbers in the varibles and also it easy to debug this way.
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default config
