a0b8d8b364c641534f957605d96f969fc5435f0b
sachin-kumar05 <sach7870@gmail.com>
2026-01-30 03:47:26 +0530

    feat: create index file to export core components for easier imports

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
+import Container from "./container/Container"
+import LogoutBtn from "./header/LogoutBtn"
+import Logo from "./Logo"
+import Button from "./header/Button"
+import Input from './Input'
+import Select from './Select'
+
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    Button,
+    Input,
+    Select
+}
