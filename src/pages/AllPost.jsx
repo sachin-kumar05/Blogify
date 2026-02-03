e3a2718a82f9db7e406395a2ed5ea016bae6f47b
sachin-kumar05 <sach7870@gmail.com>
2026-02-03 16:09:07 +0530

    feat: add pages for AddPost, AllPost, EditPost, Home, Login, Signup, and Post

---
+++ src/pages/AllPost.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import databaseService from "../appwrite/database.service"
+import { Container, PostCard } from "../components"
+
+function AllPost() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    databaseService.getPosts().then((posts) => {
+    if(posts) {
+        setPosts(posts.rows)
+    }
+  })
+  }, [])
+
+  
+  return (
+    <div className="w-full py-8">
+        <Container>
+            <div className="flex flex-wrap">
+                {posts.map((post) => (
+                    <div key={post.$id} className="p-2 w-1/4">
+                        <PostCard post={post}/>
+                    </div>
+                ))}
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default AllPost
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect} from 'react'
+import { Container, PostForm } from '../components'
+import databaseService from '../appwrite/database.service'
+import { useParams, useNavigate } from 'react-router-dom'
+
+function EditPost() {
+
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(slug) {
+            databaseService.getPost(slug).then((post) => {
+                if(post) {
+                    setPost(post)
+                }
+            })
+        } else {
+            navigate("/")
+        }
+    }, [slug, navigate])
+
+  return post ? (<div className='py-8'>
+    <Container>
+        <PostForm post={post}/>
+    </Container>
+  </div>): null
+}
+
+export default EditPost
