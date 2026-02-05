f84bd0af4b9ae9a52d45cc079cfdfc8317eff86a
sachin-kumar05 <sach7870@gmail.com>
2026-02-05 16:40:21 +0530

    fix: return user data in getCurrentUser method instead of void

--- src/appwrite/auth.service.js
+++ src/appwrite/auth.service.js
@@ -47,7 +47,7 @@ export class AuthService {
 
     async getCurrentUser() {
         try {
-            await this.account.get();
+            return await this.account.get();
         } catch (error) {
             console.log("Appwrite getCurrentUser failed:", error)
         }
